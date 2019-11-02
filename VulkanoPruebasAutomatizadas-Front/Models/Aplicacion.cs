64e22d5e25287a711020091f8f072c9a73e797d8
Carlos Blanco <Takezo316@users.noreply.github.com>
2019-11-02 01:12:16 -0500

    Add files via upload

--- VulkanoPruebasAutomatizadas-Front/Models/Aplicacion.cs
+++ VulkanoPruebasAutomatizadas-Front/Models/Aplicacion.cs
@@ -1,12 +1,30 @@
-﻿namespace VulkanoPruebasAutomatizadas_Front.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VulkanoPruebasAutomatizadas_Front.Models
 {
     public class Aplicacion
     {
         public int Aplicacion_ID { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre Aplicación")]
         public string Nombre { get; set; }
+
+        [Required]
+        [Display(Name = "Versión de la aplicación")]
         public string Version { get; set; }
+
+        [Display(Name = "Ruta de la aplicación")]
         public string Ruta_Aplicacion { get; set; }
+        
+        [Display(Name = "Es aplicación Web?")]
         public bool Es_Web { get; set; }
+
+        [Display(Name = "Descripción")]
         public string Descripcion { get; set; }
     }
 }
--- VulkanoPruebasAutomatizadas-Front/Models/Herramienta.cs
+++ VulkanoPruebasAutomatizadas-Front/Models/Herramienta.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace VulkanoPruebasAutomatizadas_Front.Models
 {
     public class Herramienta
     {
-        public int ID { get; set; }
+        public int Herramienta_ID { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre Herramienta")] 
         public string Nombre { get; set; }
 
-
+        [Required]
+        [Display(Name = "Tipo de Prueba")]
+        public string Tipo_Prueba { get; set; }
+        [Required]
+        [Display(Name = "Es Web?")]
+        public bool Es_Web { get; set; }
     }
 }
