9275d6fae3861e8617bbf8b91b63e3359902967c
Jose Laverde <jf.laverde@uniandes.edu.co>
2019-11-16 19:42:53 -0500

    Se cambia el agregar estrategia

--- VulkanoPruebasAutomatizadas-Front/Models/TipoPrueba.cs
+++ VulkanoPruebasAutomatizadas-Front/Models/TipoPrueba.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace VulkanoPruebasAutomatizadas_Front.Models
@@ -24,6 +25,18 @@ namespace VulkanoPruebasAutomatizadas_Front.Models
         [Required]
         [Display(Name = "Tipo de prueba")]
         public MQTipoPrueba MQTipoPrueba { get; set; }
+
+        [Required]
+        [StringLength(10, MinimumLength = 1)]
+        [Display(Name = "Cantidad Ejecuciones")]
+        public int CantidadEjecuciones { get; set; }
+        [Required]
+        [Display(Name = "Tiempo Ejecucion")]
+        public DateTime TiempoEjecucion { get; set; }
+        [Required]
+        [Display(Name = "Semilla")]
+        [StringLength(10, MinimumLength = 1)]
+        public string Semilla { get; set; }
 
         public List<HistorialEjecucionPrueba> HistorialEjecuciones { get; set; }
         public TipoPrueba()
