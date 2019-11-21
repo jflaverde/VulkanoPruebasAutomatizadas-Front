a0720e369699630aed78df23a079b909f2edfd35
Carlos Blanco <Takezo316@users.noreply.github.com>
2019-11-20 20:33:45 -0500

    Add files via upload

---
+++ VulkanoPruebasAutomatizadas-Front/Models/AppVersion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VulkanoPruebasAutomatizadas_Front.Models
+{
+    public class AppVersion
+    {
+        public int AppVersion_ID { get; set; }
+
+        [Required]
+        [StringLength(12, MinimumLength = 5)]
+        [Display(Name = "Número de la Version")]
+        public string Numero { get; set; }
+    }
+}
